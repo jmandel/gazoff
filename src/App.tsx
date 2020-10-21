30187c322642ce41853f147c8ecac42a753ff65d
Josh Mandel <jmandel@med.harvard.edu>
2020-10-20 19:54:55 -0500

    Psychmetric basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
+import React, {useLayoutEffect, useRef} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index'
+import {observer} from 'mobx-react';
+import {State } from './index'
 
-function App() {
+const App = observer(({store}: {store: State}) => {
+  const oRef = useRef<HTMLDivElement|null>(null);
+
+  useLayoutEffect(() => {
+    if (oRef.current) {
+      console.log("oref", oRef.current.clientWidth)
+    }
+  })
+  
+  const glassesAvg = store.avg("glasses")
+  const uncorrectedAvg = store.avg("uncorrected")
+
+  let trial: null | typeof store.trials[number] = null;
+  if (typeof store.currentStatus == "object")
+     trial = store.trials[store.currentStatus.awaiting];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="App"
+    onTouchEnd={(e) => {
+      if (!oRef.current) {
+        console.log("No oref")
+        return
+      }
+      const {clientX: x, clientY: y} = e.changedTouches[0]
+      const targetX = (trial?.x || 0) * oRef.current!.offsetParent!.clientWidth;
+      const targetY = (trial?.y || 0) * oRef.current!.offsetParent!.clientHeight;
+
+      const distance = Math.sqrt(
+        Math.pow((targetX - x), 2) +
+        Math.pow((targetY - y), 2)
+      )
+
+      console.log(x, targetX, y, targetY, distance)
+
+      store.completeTrial(distance)
+    }}
+    style={{
+      width: "100%",
+      height: "100%",
+      maxHeight: "100%",
+      position: "relative"
+    }}>
+      Data collection {(store.fractionComplete * 100).toFixed(0)}% complete.
+      <br></br>
+      {typeof store.currentStatus == "object" && <span 
+        style={{fontStyle: "italic"}}
+      >
+        Please hold device at {store.trials[store.currentStatus.awaiting].clockPosition} o'clock .
+        <br></br>
+        Please say "{store.trials[store.currentStatus.awaiting].type}".
+       </span>}
+      <br></br>
+ 
+      {store.currentStatus == "paused" && <button
+        onClick={()=>{
+          store.resume()
+        }}
+      >
+        Begin 
+       </button>}
+      {
+        !isNaN(glassesAvg + uncorrectedAvg) && 
+        <div>
+          <br></br>
+            Glasses {glassesAvg.toFixed(0)}  vs Uncorrected  {uncorrectedAvg.toFixed(0)}.
+          <br></br>
+            T-test result: p={(store.stat)}.
+        </div>
+      }
+
+      <div 
+      ref={oRef}
+      className="marks-the-spot"
+      style={{
+        display: "absolute",
+        left: (trial?.x || .5) * 100 + "%",
+        top: (trial?.y || .5) * 100 + "%",
+        textAlign: "left",
+        color: "blue"
+      }}>o</div>
+
+      <pre style={{
+        maxWidth: "100%",
+        overflow: "scroll"
+      }}>
+        {JSON.stringify(store.trials)}
+      </pre>
+      
     </div>
   );
-}
+});
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,90 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { makeAutoObservable, makeObservable, observable } from "mobx";
+
+// @ts-ignore
+import ttest from 'ttest'
+
+export interface State {
+  readonly fractionComplete: number,
+  stat: number,
+  completeTrial: (distance: number) => void,
+  resume: () => void,
+  avg: (type: "glasses" | "uncorrected") => number,
+  currentStatus: "paused" | "finished" | { awaiting: number, since: Date }
+  trials: {
+    type: "glasses" | "contacts" | "uncorrected",
+    clockPosition: number,
+    x: number,
+    y: number,
+    tapDistance: null | number
+  }[]
+}
+
+function createState({nTrials} = {
+  nTrials: 5
+}): State {
+  return {
+    avg(type) {
+      const distances = (type: "glasses" | "uncorrected") => this.trials.filter(t => t.type === type && t.tapDistance).map(t => t.tapDistance)
+      const td = distances(type) as number[]
+      return td.reduce((tot, d) => tot+d, 0) / td.length
+    },
+    get stat() {
+      const distances = (type: "glasses" | "uncorrected") => this.trials.filter(t => t.type === type && t.tapDistance).map(t => t.tapDistance)
+      const d1 = distances("glasses")
+      const d2 = distances("uncorrected")
+      if (d1.length < 1 || d2.length < 1) {
+        return 0
+      }
+      const test = ttest(d1, d2)
+      return test.pValue() as number
+    },
+    resume() {
+      this.currentStatus = {
+        awaiting: this.trials.filter(t => t.tapDistance).length,
+        since: new Date()
+      }
+    },
+    currentStatus: "paused",
+    completeTrial(distance) {
+      if (typeof this.currentStatus === 'object') {
+        this.trials[this.currentStatus.awaiting].tapDistance = distance;
+        let nextTrial = this.currentStatus.awaiting + 1
+        if (nextTrial < this.trials.length) {
+          this.currentStatus = {
+            awaiting: nextTrial,
+            since: new Date()
+          }
+        } else {
+          this.currentStatus = "finished"
+        }
+      }
+    },
+    get fractionComplete() {
+      const trials = this.trials;
+      const completed = trials.filter(t => t.tapDistance !== null)
+      return completed.length / trials.length
+    },
+    trials: Array(nTrials).fill(0).map((v, i) => ({
+      type: i < nTrials/2 ? "glasses" : "uncorrected",
+      clockPosition: Math.floor(Math.random() * 11 + 1),
+      tapDistance:null,
+      x: Math.random(),
+      y: Math.random()
+    }))  
+  }
+}
+
+let state = makeAutoObservable(createState({
+  nTrials: 20
+}));
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
